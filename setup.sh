1f6ffbde7d6cd5170aca8cf36053ad600f4211b7
William Lu <wy.william.lu@gmail.com>
2024-08-20 19:30:05 +0800

    Add support for sysvinit and systemd
    Enable init scripts for sysvinit and systemd when installing
    Provide setup.conf for sysvinit and systemd
    Add init_system argument to ./setup.sh install command
    Move dev_init_dir back ino setup.conf

--- setup.sh
+++ setup.sh
@@ -9,7 +9,11 @@ install() {
     cp lib/setup-config /usr/lib/setup-config
 
     if [ ! -f /etc/setup.conf ]; then
-        cp etc/setup.conf.sample /etc/setup.conf
+        if [ "$init_system" = "systemd" ]; then
+            cp etc/setup.conf.systemd.sample /etc/setup.conf
+        elif [ "$init_system" = "sysvinit" ]; then
+            cp etc/setup.conf.sysvinit.sample /etc/setup.conf
+        fi
     fi
 
     echo "Done"
@@ -21,21 +25,31 @@ uninstall() {
 
     rm -f /usr/lib/setup-config
 
+    # Avoid removing /etc/setup.conf and preserve relative administration efforts
+
     echo "Done"
 }
 
 usage() {
     /bin/echo -e "Usage:"
-    /bin/echo -e "\t./setup.sh install"
+    /bin/echo -e "\t./setup.sh install <init_system>"
     /bin/echo -e "\t./setup.sh uninstall"
+    /bin/echo -e "Arguments:"
+    /bin/echo -e "\tinit_system\tthe value be systemd or sysvinit"
 }
 
-if [ "$#" -ne 1 ]; then
+if [ "$#" -lt 1 ] || [ "$#" -gt 2 ]; then
     usage
     exit 1
 elif [ "$1" = "install" ]; then
-    install
-elif [ "$1" = "uninstall" ]; then
+    if [ "$2" = "systemd" ] || [ "$2" = "sysvinit" ]; then
+        init_system="$2"
+        install
+    else
+        usage
+        exit 1
+    fi
+elif [ "$1" = "uninstall" ] && [ "$2" = "" ]; then
     uninstall
 else
     usage
