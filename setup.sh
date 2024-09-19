f4fa5e9594d38a430aab4cbc8c4333a22096eeb7
William Lu <wy.william.lu@gmail.com>
2024-09-19 18:57:57 +0800

    Add a --root-dir option to setup.sh
    Add a sys_root_dir variable
    Generate bin files from templates

--- setup.sh
+++ setup.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 
 install() {
+    init_sys_dirs
     pre_install
 
     echo "Install: started"
@@ -14,25 +15,54 @@ install() {
 }
 
 pre_install() {
-    PKG_INIT_DIR="$init_system"
+    generate_bin_files
+    generate_etc_files
+}
+
+generate_bin_files() {
+    mkdir -p "$pkg_bin_dir"
+    render_template "$pkg_bin_dir/setup" "SYS_ROOT_DIR=$sys_root_dir"
+    render_template "$pkg_bin_dir/setup-env" "SYS_ROOT_DIR=$sys_root_dir"
+}
+
+generate_etc_files() {
+    local pkg_init_dir="$init_system"
+    local sys_init_dir
     if [ "$init_system" = "systemd" ]; then
-        SYS_INIT_DIR="/lib/systemd/system"
+        sys_init_dir="/lib/systemd/system"
     elif [ "$init_system" = "sysvinit" ]; then
-        SYS_INIT_DIR="/etc/init.d"
+        sys_init_dir="/etc/init.d"
     elif [ "$init_system" = "procd" ]; then
-        SYS_INIT_DIR="/etc/init.d"
+        sys_init_dir="/etc/init.d"
     fi
+
     mkdir -p "$pkg_etc_dir"
-    sed \
-        -e "s;{PKG_INIT_DIR};$PKG_INIT_DIR;g" \
-        -e "s;{SYS_INIT_DIR};$SYS_INIT_DIR;g" \
-        "template/setup.conf.sample" \
-        >"etc/setup.conf.sample"
+    render_template "$pkg_etc_dir/setup.conf.sample" \
+        "SYS_ROOT_DIR=$sys_root_dir" \
+        "PKG_INIT_DIR=$pkg_init_dir" \
+        "SYS_INIT_DIR=$sys_init_dir"
+}
+
+render_template() {
+    local template_file="$1"
+    shift
+
+    get_replace_options "$@" | xargs -d '\n' sed "template/$template_file" >"$template_file"
+}
+
+get_replace_options() {
+    for entry in "$@"; do
+        local key="${entry%%=*}"
+        local value="${entry#*=}"
+        printf "%s\n" "-e"
+        echo "s;\${$key};$value;g"
+    done
 }
 
 uninstall() {
+    init_sys_dirs
     # Check packages in system
-    local packages=$(ls -1 /usr/lib/setup/packages)
+    local packages=$(ls -1 "$sys_pkgs_dir")
     if [ ! -z "$packages" ]; then
         echo "Error: Cannot uninstall Setup.sh before uninstalling all of the packages."
         exit 1
@@ -43,42 +73,76 @@ uninstall() {
     remove_files "$pkg_bin_dir" "$sys_bin_dir"
     remove_files "$pkg_lib_dir" "$sys_lib_dir"
     remove_files "$pkg_share_dir" "$sys_share_dir"
-    # Avoid removing /etc/setup.conf and preserve relative administration efforts
+    # Avoid removing $sys_etc_dir/setup.conf and preserve relative administration efforts
 
     echo "Uninstall: finished"
 }
 
 usage() {
     /bin/echo -e "Usage:"
-    /bin/echo -e "\t./setup.sh install <init_system>"
-    /bin/echo -e "\t./setup.sh uninstall"
+    /bin/echo -e "\t./setup.sh install <init_system> [--root-dir <dir>]"
+    /bin/echo -e "\t./setup.sh uninstall [--root-dir <dir>]"
     /bin/echo -e "\t./setup.sh version"
     /bin/echo -e "Arguments:"
-    /bin/echo -e "\tinit_system\tthe value can be: systemd|sysvinit|procd"
+    /bin/echo -e "\tinit_system\tThe value can be: systemd|sysvinit|procd"
+    /bin/echo -e "Options:"
+    /bin/echo -e "\t--root-dir\tThe root dir of installation destionation"
 }
 
-sys_bin_dir=/usr/bin
-sys_lib_dir=/usr/lib
-sys_share_dir=/usr/share
-sys_etc_dir=/etc
+init_sys_dirs() {
+    sys_bin_dir="$sys_root_dir/usr/bin"
+    sys_lib_dir="$sys_root_dir/usr/lib"
+    sys_share_dir="$sys_root_dir/usr/share"
+    sys_etc_dir="$sys_root_dir/etc"
+
+    sys_pkgs_dir="$sys_root_dir/usr/lib/setup/packages"
+}
+
+parse_root_dir_arg() {
+    if [ "$#" -gt 0 ]; then
+        require_args "$#" 2
+        if [ "$1" = "--root-dir" ]; then
+            sys_root_dir="$2"
+            shift 2
+
+            refuse_args "$#"
+        else
+            exit_with_usage
+        fi
+    fi
+}
 
 . "lib/setup/common"
 . "lib/setup/info"
 
 dry_run=false
 
-if [ "$#" -lt 1 ] || [ "$#" -gt 2 ]; then
-    exit_with_usage
-elif [ "$1" = "install" ]; then
-    if [ "$2" = "systemd" ] || [ "$2" = "sysvinit" ] || [ "$2" = "procd" ]; then
-        init_system="$2"
+require_args "$#" 1
+if [ "$1" = "install" ]; then
+    shift
+
+    require_args "$#" 1
+    if [ "$1" = "systemd" ] || [ "$1" = "sysvinit" ] || [ "$1" = "procd" ]; then
+        init_system="$1"
+        shift
+
+        parse_root_dir_arg "$@"
+
         install
     else
         exit_with_usage
     fi
-elif [ "$1" = "uninstall" ] && [ "$2" = "" ]; then
+elif [ "$1" = "uninstall" ]; then
+    shift
+
+    parse_root_dir_arg "$@"
+
     uninstall
 elif [ "$1" = "version" ]; then
+    shift
+
+    refuse_args "$#"
+
     print_setup_version
 else
     exit_with_usage
